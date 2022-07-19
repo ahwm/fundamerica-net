28894d9610123960dce764a746875ee6a75d3a19
Adam Humpherys <adam@adamh.us>
2022-07-19 09:10:24 -0600

    added tests

---
+++ FundAmerica.Tests/AsyncTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FundAmerica.Tests
+{
+    public class AsyncTests
+    {
+        // Thanks to Phil Haack
+        // https://haacked.com/archive/2014/11/11/async-void-methods/
+        [Fact]
+        public void VerifyAsyncVoid()
+        {
+            Extensions.AssertNoAsyncVoidMethods(GetType().Assembly);
+            Extensions.AssertNoAsyncVoidMethods(typeof(FundAmericaClient).Assembly);
+        }
+    }
+}
---
+++ FundAmerica.Tests/Extensions.cs
@@ -0,0 +1,60 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FundAmerica.Tests
+{
+    // Thanks to Phil Haack
+    // https://haacked.com/archive/2014/11/11/async-void-methods/
+    internal static class Extensions
+    {
+        public static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
+        {
+            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+            try
+            {
+                return assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                return e.Types.Where(t => t != null);
+            }
+        }
+
+        public static bool HasAttribute<TAttribute>(this MethodInfo method) where TAttribute : Attribute
+        {
+            return method.GetCustomAttributes(typeof(TAttribute), false).Any();
+        }
+
+        public static IEnumerable<MethodInfo> GetAsyncVoidMethods(this Assembly assembly)
+        {
+            return assembly.GetLoadableTypes()
+              .SelectMany(type => type.GetMethods(
+                BindingFlags.NonPublic
+                | BindingFlags.Public
+                | BindingFlags.Instance
+                | BindingFlags.Static
+                | BindingFlags.DeclaredOnly))
+              .Where(method => method.HasAttribute<AsyncStateMachineAttribute>())
+              .Where(method => method.ReturnType == typeof(void));
+        }
+
+        public static void AssertNoAsyncVoidMethods(Assembly assembly)
+        {
+            var messages = assembly
+                .GetAsyncVoidMethods()
+                .Select(method =>
+                    String.Format("'{0}.{1}' is an async void method.",
+                        method.DeclaringType.Name,
+                        method.Name))
+                .ToList();
+            messages.Any().ShouldBeFalse("Async void methods found!" + Environment.NewLine + String.Join(Environment.NewLine, messages));
+        }
+    }
+}
